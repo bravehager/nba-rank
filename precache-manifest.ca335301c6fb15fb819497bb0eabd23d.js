df73b6e201d6a077a76c5eca6a3d89452d7f9175
Brave Hager <bravehager7@gmail.com>
2019-09-29 05:35:30 -0400

    Updates

---
+++ precache-manifest.ca335301c6fb15fb819497bb0eabd23d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3414cba4f41531c15072a6e4c807a9a",
+    "url": "/nba-rank/index.html"
+  },
+  {
+    "revision": "807660c359a7821db793",
+    "url": "/nba-rank/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "08eee6cf8cdb880804aa",
+    "url": "/nba-rank/static/css/main.941bc53b.chunk.css"
+  },
+  {
+    "revision": "807660c359a7821db793",
+    "url": "/nba-rank/static/js/2.3f1e9d46.chunk.js"
+  },
+  {
+    "revision": "08eee6cf8cdb880804aa",
+    "url": "/nba-rank/static/js/main.b2890e97.chunk.js"
+  },
+  {
+    "revision": "394c1ba472d7a303b7be",
+    "url": "/nba-rank/static/js/runtime-main.4f9a98e8.js"
+  }
+]);
